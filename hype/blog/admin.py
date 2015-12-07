3758ef59c5d1fd801141864874f5c1b53e2e624b
Nitin Paul <npaul2811@gmail.com>
2015-12-07 21:28:02 +0530

    Post and Tag models registerd with admin. Fixes #42.

--- hype/blog/admin.py
+++ hype/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+class PostAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Tag)
