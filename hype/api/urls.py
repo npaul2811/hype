b959eb2b681600d48c83f5b254c0d617e18f1cf3
Nitin Paul <npaul2811@gmail.com>
2016-01-01 13:56:10 +0530

    Added APISignInView endpoint to the API.

--- hype/api/urls.py
+++ hype/api/urls.py
@@ -5,6 +5,7 @@ from api import views
 urlpatterns = [	
 	url(r'^$', views.APIRootView.as_view(), name='api-root'),
 	url(r'^signup/$', views.APISignUpView.as_view(), name='api-signup'),
+	url(r'^signin/$', views.APISignInView.as_view(), name='api-signin'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- hype/api/views.py
+++ hype/api/views.py
@@ -40,7 +40,7 @@ class APISignUpView(APIView):
 		password = request.POST['password']
 		user = authenticate(username=username, password=password)
 		if user is not None:
-			message = 'Credentials already exist. Use APIAuthView to authenticate.'
+			message = 'Credentials already exist. Use APISignInView to authenticate.'
 			token = 'None'
 		else:
 			newremote = User.objects.create_user(username, None, password)
@@ -59,3 +59,34 @@ class APISignUpView(APIView):
 		data = json.dumps(message_dump)
 		return Response(data, content_type='application/json')
 
+class APISignInView(APIView):
+	"""
+	API endpoint to authenticate a remote client and generate auth token.
+	"""
+	def get(self, request, format=None):
+		message_dump = {
+			'message': 'POST client credentials to authenticate.'
+		}
+		data = json.dumps(message_dump)
+		return Response(data, content_type='application/json')
+
+	def post(self, request, format=None):
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				message = 'Remote client authenticated.'
+				authtoken = Token.objects.get_or_create(user=user)
+				token = authtoken[0].key
+		else:
+			message = 'Login credentials not found. Sign upusing APISignUpView endpoint.'
+			token = 'None'
+
+		message_dump = {
+			'message': message,
+			'token': token
+		}
+		data = json.dumps(message_dump)
+		return Response(data, content_type='application/json')
