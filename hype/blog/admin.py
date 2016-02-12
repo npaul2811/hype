c234a8522cb2a96f84df879703e926ed718960eb
ffx01 <walters.justin01@gmail.com>
2016-02-11 22:39:20 -0800

    V0.1.1 - refactored blog/admin

--- hype/blog/admin.py
+++ hype/blog/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 from . import models
 
-class PostAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('title',)}
 
-admin.site.register(models.Post, PostAdmin)
-admin.site.register(models.Tag)
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
