5a6bf73dd5c53b8b006d094f7c3c1ca890050294
Nitin Paul <npaul2811@gmail.com>
2016-02-15 20:44:10 +0530

    Added test to check basic URL mapping

--- hype/blog/tests.py
+++ hype/blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+	def setUp(self):
+		pass
+
+	def test_index_is_mapped_properly(self):
+		"""Tests whether index is mapped properly"""
+		c = Client()
+		response = c.get('/')
+		self.assertEqual(response.status_code, 200)
