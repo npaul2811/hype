65fc11849c428b279fc898353c96e8587bb8ab90
Nitin Paul <npaul2811@gmail.com>
2016-02-17 19:49:42 +0530

    Added test to check auth while accessing API endpoints

--- hype/api/tests.py
+++ hype/api/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.test import Client
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 class APITestCase(TestCase):
 	def setUp(self):
@@ -11,3 +13,14 @@ class APITestCase(TestCase):
 		c = Client()
 		response = c.get('/api')
 		self.assertEqual(response.status_code, 301)
+
+	def test_API_works_with_authentication(self):
+		"""Tests whether API endpoints are visible when
+		authenticated."""
+		User.objects.create_user('TestUser', None, 'TestPass')
+		user = authenticate(username='TestUser', password='TestPass')
+		c = Client()
+		c.login(username='TestUser', password='TestPass'):
+		response = c.get('/api')
+		self.assertEqual(response.status_code, 200)
+		
