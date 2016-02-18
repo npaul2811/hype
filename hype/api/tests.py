6d48dfd55b7ea1e61b487e1bbbddb4abc1d0116d
Nitin Paul <npaul2811@gmail.com>
2016-02-18 21:20:15 +0530

    Fixed bugs in API tests.py

--- hype/api/tests.py
+++ hype/api/tests.py
@@ -1,26 +1,30 @@
 from django.test import TestCase
 from django.test import Client
+from rest_framework.test import APIClient
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 
 class APITestCase(TestCase):
 	def setUp(self):
-		pass
+		user = User.objects.create_user('TestUser', None, 'TestPass')
+		user.save()
 
-	def test_API_checks_for_authentication(self):
-		"""Tests whether API checks for authentication
-		and return 301 if not authenticated."""
-		c = Client()
-		response = c.get('/api')
-		self.assertEqual(response.status_code, 301)
+	def test_API_checks_auth(self):
+		"""
+		Tests if API root throws 403(Forbidden) if not authenticated
+		"""
+		client = APIClient()
+		response = client.get('/api/')
+		self.assertEqual(response.status_code, 403)
 
 	def test_API_works_with_authentication(self):
-		"""Tests whether API endpoints are visible when
-		authenticated."""
-		User.objects.create_user('TestUser', None, 'TestPass')
-		user = authenticate(username='TestUser', password='TestPass')
-		c = Client()
-		c.login(username='TestUser', password='TestPass'):
-		response = c.get('/api')
+		"""
+		Tests whether API endpoints are visible when authenticated
+		"""	
+		client = Client()	
+		rest_client = APIClient()
+		client.login(username='TestUser', password='TestPass')
+		rest_client.login(username='TestUser', password='TestPass')
+		response = rest_client.get('/api/posts/', format = 'json')
 		self.assertEqual(response.status_code, 200)
 		
